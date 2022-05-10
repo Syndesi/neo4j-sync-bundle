ffbe5341728a2498f62d9a1d740d8348ef3dd03a
Sören Klein <soerenklein98@protonmail.com>
2022-05-10 15:49:10 +0200

    wip

--- src/Command/DatabasePurgeCommand.php
+++ src/Command/DatabasePurgeCommand.php
@@ -4,31 +4,60 @@ declare(strict_types=1);
 
 namespace Syndesi\Neo4jSyncBundle\Command;
 
-use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Question\ConfirmationQuestion;
+use Symfony\Component\Console\Style\SymfonyStyle;
 use Syndesi\Neo4jSyncBundle\Contract\Neo4jClientInterface;
+use Syndesi\Neo4jSyncBundle\Statement\DeleteAllNodesAndRelationsStatementBuilder;
 
 class DatabasePurgeCommand extends Command
 {
     protected static $defaultName = 'neo4j-sync:db:purge';
 
-    private EntityManagerInterface $em;
-    private Neo4jClientInterface $client;
-
     public function __construct(
-        EntityManagerInterface $em,
-        Neo4jClientInterface $client
+        private Neo4jClientInterface $client
     ) {
-        $this->em = $em;
-        $this->client = $client;
         parent::__construct();
+    }
+
+    protected function configure()
+    {
+        $this
+            ->addOption(
+                'force',
+                'f',
+                InputOption::VALUE_NEGATABLE,
+                'Disables sanity check, executes without questions',
+                false
+            );
     }
 
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
-        $output->writeln('Hello world :D');
+        $io = new SymfonyStyle($input, $output);
+
+        $helper = $this->getHelper('question');
+        $question = new ConfirmationQuestion(("Purge all nodes and relations? (yes/NO): "), false);
+        if (!$input->getOption('force')) {
+            if (!$helper->ask($input, $output, $question)) {
+                $io->writeln("Canceled execution");
+
+                return Command::SUCCESS;
+            }
+        } else {
+            $io->writeln("Skipped confirmation question due to force flag");
+        }
+
+        $io->write("Purging all nodes and relations...");
+        $this->client->addStatements(DeleteAllNodesAndRelationsStatementBuilder::build());
+        $this->client->flush();
+        $io->writeln(" done");
+
+        $io->newLine();
+        $io->writeln(sprintf("To purge indices please run command \"%s\"", IndexPurgeCommand::getDefaultName()));
 
         return Command::SUCCESS;
     }
---
+++ src/Statement/DeleteAllNodesAndRelationsStatementBuilder.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Syndesi\Neo4jSyncBundle\Statement;
+
+use Laudis\Neo4j\Databags\Statement;
+use Syndesi\Neo4jSyncBundle\Contract\StatementBuilderInterface;
+
+class DeleteAllNodesAndRelationsStatementBuilder implements StatementBuilderInterface
+{
+    /**
+     * @return Statement[]
+     */
+    public static function build(): array
+    {
+        return [new Statement(
+            "MATCH (n)\n".
+            "DETACH DELETE n",
+            []
+        )];
+    }
+}
